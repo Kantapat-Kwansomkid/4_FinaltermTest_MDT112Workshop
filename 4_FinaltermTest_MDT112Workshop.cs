a0ffdbfe6ce876ecfa3f7da042089fc31dc98bbe
Kantapat-Kwansomkid <72593812+Kantapat-Kwansomkid@users.noreply.github.com>
2021-06-13 16:58:58 +0700

    Add files via upload

---
+++ 4_FinaltermTest_MDT112Workshop.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _4_FinaltermTest_MDT112Workshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("");
+            a = int.Parse(Console.ReadLine());
+
+            string b;
+            Console.Write("");
+            b = Console.ReadLine();
+
+            int c;
+            Console.Write("");
+            c = int.Parse(Console.ReadLine());
+
+            int d;
+            Console.Write("");
+            d = int.Parse(Console.ReadLine());
+
+            int e;
+            Console.Write("");
+            e = int.Parse(Console.ReadLine());
+
+            int f;
+            Console.Write("");
+            f = int.Parse(Console.ReadLine());
+
+            int g;
+            Console.Write("");
+            g = int.Parse(Console.ReadLine());
+
+            string h;
+            Console.Write("");
+            h = Console.ReadLine();
+
+            int i;
+            Console.Write("");
+            i = int.Parse(Console.ReadLine());
+
+            int j;
+            Console.Write("");
+            j = int.Parse(Console.ReadLine());
+
+            int k;
+            Console.Write("");
+            k = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Invalid ID");
+        }
+    }
+}
